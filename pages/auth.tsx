8725cf4c9466019cc4f68b81fb0961406b88b5ff
James Niyongira <lascasasjames85@gmail.com>
2023-03-30 16:13:23 -0400

    added a logout functionality

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -5,6 +5,9 @@ import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 import { redirect } from "next/dist/server/api-utils";
 
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
 const Auth = () => {
   const router = useRouter();
   const [email, setEmail] = useState("");
@@ -82,6 +85,19 @@ const Auth = () => {
             >
               {variant === "login" ? "Sign in" : "Register"}
             </button>
+
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
+
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "Don't have an account? "
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix clone</h1>
+      <h1 className="text-4xl text-green-500">Netflix clone</h1>
+      <button
+        onClick={() => signOut()}
+        className="bg-red-600 text-white h-10  rounded-md w-full hover:bg-red-700 transition font-bold"
+      >
+        Sign out
+      </button>
     </>
   );
 }
