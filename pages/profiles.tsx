bc5d6164a197ea74eec9239dcec5350db8cf9104
James Niyongira <lascasasjames85@gmail.com>
2023-03-30 17:24:51 -0400

    added user profile

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -17,8 +17,21 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 const profiles = () => {
   return (
-    <div>
-      <p className="text-white text-4xl">Profiles</p>
+    <div className="flex items-center h-full justify-center">
+      <div className="flex flex-col ">
+        <h1 className="text-3xl md:text-6xl text-white text-center">
+          Who is watching?
+        </h1>
+        <div className="flex items-center justify-center gap-8 mt-10">
+          <div onClick={() => {}}>
+            <div className="group flex-row w-44 mx-auto">
+              <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white transition overflow-hidden">
+                <img src="/images/default-blue.png" alt="profile" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
